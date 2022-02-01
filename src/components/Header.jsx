31190f643bf116e1000b4392660c2c574d2b70a7
MaxtorSan <pnv2k15@gmail.com>
2022-02-01 20:47:58 +0300

    test

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+       
+        <img src='https://static.vecteezy.com/system/resources/previews/002/270/921/non_2x/community-group-family-and-social-logo-free-vector.jpg' />
+        
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+      <img src='https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg' />
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      My posts
+      <div>
+        New post
+      </div>
+      <div>
+        post1
+      </div>
+      <div>
+        post2
+      </div>
+    </div>
+    <div>
+      <img src='https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmV3JTIwemVhbGFuZCUyMHBlb3BsZXxlbnwwfHwwfHw%3D&w=1000&q=80' />
+    </div>
+</div>
+}
+
+export default Profile;
